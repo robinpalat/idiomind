b2fce8d301458fc10528bd3b43d21187f193be32
robinsato <robinpalat@gmail.com>
2015-02-02 11:57:52 -0300

    fix

--- practice/hint.sh
+++ practice/hint.sh
@@ -2,14 +2,13 @@
 # -*- ENCODING: UTF-8 -*-
 
 source /usr/share/idiomind/ifs/c.conf
-
 eyeD3 "$DM_tlt/$(sed -n "$1"p lsin)".mp3 | \
 grep -o -P '(?<=ISI1I0I).*(?=ISI1I0I)' \
 | awk '{print tolower($0)}' | sed "s/\b\(.\)/\u\1/g" \
-| sed "s|\.||g" | sed "s|[a-z]|"\."|g" | sed "s| |\t|g" \
+| sed "s|\.||; s|\,||; s|\;||g" | sed "s|[a-z]|"\."|g" | sed "s| |\t|g" \
 | sed "s|\.|\ .|g" | yad --center --text-info --skip-taskbar \
---justify=left --margins=10 --fontname="Verdana Black" \
+--justify=left --margins=15 --fontname="Verdana Black" \
 --buttons-layout=end --borders=0 --wrap --title=" " \
---text-align=center --height=180 --width=460 \
+--text-align=center --height=150 --width=460 \
 --on-top --align=center --window-icon=idiomind \
---button="gtk-close:0" & exit
+--no-buttons & exit
