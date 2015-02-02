0d4009e2139e08f85e28f30f74ea0b47f2c806e1
robinsato <robinpalat@gmail.com>
2015-02-02 10:57:16 -0300

    fix

--- practice/hint.sh
+++ practice/hint.sh
@@ -2,13 +2,14 @@
 # -*- ENCODING: UTF-8 -*-
 
 source /usr/share/idiomind/ifs/c.conf
-s1=$(sed -n "$1"p lsin)
-prsw=$(eyeD3 "$DM_tlt/$s1".mp3 | grep -o -P '(?<=ISI1I0I).*(?=ISI1I0I)' \
+
+eyeD3 "$DM_tlt/$(sed -n "$1"p lsin)".mp3 | \
+grep -o -P '(?<=ISI1I0I).*(?=ISI1I0I)' \
 | awk '{print tolower($0)}' | sed "s/\b\(.\)/\u\1/g" \
-| sed "s|[a-z]|"\*"|g" | sed "s| |\t|g")
-echo "$prsw" | yad --center --text-info --skip-taskbar \
---justify=left --margins=5 --fontname=verdana \
+| sed "s|\.||g" | sed "s|[a-z]|"\."|g" | sed "s| |\t|g" \
+| sed "s|\.|\ .|g" | yad --center --text-info --skip-taskbar \
+--justify=left --margins=10 --fontname="Verdana Black" \
 --buttons-layout=end --borders=0 --wrap --title=" " \
 --text-align=center --height=180 --width=460 \
 --on-top --align=center --window-icon=idiomind \
---button="gtk-close:0" &
+--button="gtk-close:0" & exit
