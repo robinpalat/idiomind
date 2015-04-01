e6ea5587dbe27e63c146067b3568cc283d3dbc21
rovym <robinpalat@gmail.com>
2015-04-01 09:37:41 -0300

    --

--- ifs/mods/start/weekly_report.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-
-source /usr/share/idiomind/ifs/c.conf
-if ([ "$(date +%u)" = '4' ] && \
-	[ "$(sed -n 1p $DC_a/stats/cnfg)" = "TRUE" ]); then
-	(sleep 10 && "$DS/addons/weekly report/cnfg.sh" A) &
-fi
