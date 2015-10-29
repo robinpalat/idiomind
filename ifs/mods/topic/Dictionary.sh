4999dc215b50d591ed7187d81e2b412ef560186c
robinpalat <robinpalat@gmail.com>
2015-10-28 21:14:42 -0300

    update

--- ifs/mods/topic/Dictionary.sh
+++ ifs/mods/topic/Dictionary.sh
@@ -13,23 +13,22 @@ function Dictionary() {
             read word <&3
             read tran <&4
             if [ -n "$word" -a -n "$tran" ]; then
-                echo "<span font_desc='Free Sans 11'>$word</span>"
-                echo "<span font_desc='Free Sans 11'>$tran</span>"
+                echo "<span font_desc='Arial Bold 15'>$word</span>"
+                echo "<span font_desc='Arial 15'>$tran</span>"
             fi
             [ ${limit} -gt 200 ] && break
             let limit++
         done
     }
     list | yad --list --title="$(gettext "New Words")" \
-    --print-all \
     --dclick-action="$DS/play.sh play_word" \
     --search-column=1 --hide-column=3 --regex-search \
-    --column="$(gettext "$lgtl")                                                                ":TEXT \
+    --column="$(gettext "$lgtl")                                                                          ":TEXT \
     --column="$(gettext "$lgsl")":TEXT \
     --name=Idiomind --class=Idiomind  \
     --center --align=right --ellipsize=END --image-on-top \
     --window-icon=idiomind --center \
-    --width=620 --height=580 --borders=8 \
+    --width=620 --height=580 --borders=10 \
     --button="gtk-close":1
     
 } >/dev/null 2>&1
