7cd8c8b8cf3907e17dfbc26abb07b51c9077741e
robinpalat <robinpalat@gmail.com>
2015-12-11 14:50:54 -0300

    update

--- ifs/mods/add/add.sh
+++ ifs/mods/add/add.sh
@@ -627,6 +627,7 @@ function dlg_text_info_3() {
     rm -f "${DC_tlt}"/err
 }
 
+
 function dlg_form_3() {
     yad --form --title=$(gettext "Image") "$image" "$label" \
     --name=Idiomind --class=Idiomind \
