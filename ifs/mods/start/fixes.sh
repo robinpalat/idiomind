1897795ba4df113a7014b0d4ceb69ed9782158f9
robinpalat <robinpalat@gmail.com>
2016-02-07 01:21:21 -0300

    update

--- ifs/mods/start/fixes.sh
+++ ifs/mods/start/fixes.sh
@@ -2,13 +2,18 @@
 
 $DS_a/Dics/cnfg.sh updt_dicts &
 
-
 if [ ! -d "$DM_tl/.share/data" ]; then
-    mv -f "$DM_tl/.share/Dictionary" "$DM_tl/.share/data"
-    [ -d "$DM_tl/.share/data" ] && rm -r "$DM_tl/.share/data/.conf"
+
+    if [ -d "$DM_tl/.share/Dictionary" ]; then
+        [ -d "$DM_tl/.share/Dictionary/.conf" ] && \
+        rm -r "$DM_tl/.share/Dictionary/.conf"
+        mv -f "$DM_tl/.share/Dictionary" "$DM_tl/.share/data"
+    else
+        mkdir "$DM_tl/.share/data"; fi
+
     for n in {0..6}; do
-        if [ -e "$DM_tl/.${n}" ]; then
-            mv "$DM_tl/.${n}" "$DM_tl/.share/${n}"
+        if [ -e "$DM_tl/.${n}.cfg" ]; then
+            mv "$DM_tl/.${n}.cfg" "$DM_tl/.share/${n}.cfg"
         fi
     done
 fi
