fbb1d2583424319d5ac472d765214d5ab7951b6c
robinpalat <robinpalat@gmail.com>
2016-02-08 15:07:38 -0300

    update

--- ifs/stats/stats.sh
+++ ifs/stats/stats.sh
@@ -36,7 +36,7 @@ function stats() {
     --name=Idiomind --class=Idiomind \
     --orient=vert --window-icon=idiomind --on-top --center \
     --gtkrc="$DS/default/gtkrc.cfg" \
-    --width=620 --height=420 --borders=2 --splitter=280 \
+    --width=650 --height=420 --borders=2 --splitter=280 \
     --button="<small>$(gettext "Close")</small>":1
 } >/dev/null 2>&1
 
