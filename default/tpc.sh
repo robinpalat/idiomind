491f5047d3d115ff86159fdca02d4fbad2393760
robinpalat <robinpalat@gmail.com>
2016-02-20 17:16:09 -0300

    update

--- default/tpc.sh
+++ default/tpc.sh
@@ -21,30 +21,10 @@ checkt() {
         echo " " > "${DC_tlt}/info"
         echo 1 > "8.cfg"; cd /
     fi
+    
     if [ ! -f "$DT/.n_s_pr" ]; then
-            "$DS/ifs/tls.sh" check_index "${topic}"; fi
-        if [[ $(grep -Fxon "${topic}" "$DM_tl/.share/1.cfg" \
-        | sed -n 's/^\([0-9]*\)[:].*/\1/p') -ge 100 ]]; then
-            stts=$(sed -n 1p "${DC_tlt}/8.cfg")
-        
-            if [ -f "${DC_tlt}/9.cfg" ]; then
-                calculate_review "${topic}"
-                if [[ $((stts%2)) = 0 ]]; then
-                    if [ ${RM} -ge 180 -a ${stts} = 8 ]; then
-                        echo 10 > "${DM_tlt}/.conf/8.cfg"; touch "${DM_tlt}"
-                    elif [ ${RM} -ge 100 -a ${stts} -lt 8 ]; then
-                        echo 8 > "${DM_tlt}/.conf/8.cfg"; touch "${DM_tlt}"
-                    fi
-                else
-                    if [ ${RM} -ge 180 -a ${stts} = 7 ]; then
-                        echo 9 > "${DM_tlt}/.conf/8.cfg"; touch "${DM_tlt}"
-                    elif [ ${RM} -ge 100 -a ${stts} -lt 7 ]; then
-                        echo 7 > "${DM_tlt}/.conf/8.cfg"; touch "${DM_tlt}"
-                    fi
-                fi
-            fi
-            "$DS/mngr.sh" mkmn 1
-        fi
+        "$DS/ifs/tls.sh" check_index "${topic}"
+    fi
 }
 
 shakeit() {
@@ -72,13 +52,32 @@ if [ -d "${DM_tlt}" ]; then
         
      elif [ ${mode} = 12 ]; then
      
-        msg_2 "$(gettext "Topic considered inactive \(due to it has not been used for more than 80 days\). Do you want to enable it now?")" info "$(gettext "Yes")" "$(gettext "No")"
+        msg_2 "$(gettext "Topic considered inactive \(due to it has not been used for more than 80 days\). Do you want to enable it now?")" \
+        info "$(gettext "Yes")" "$(gettext "No")"
         
         if [ $? = 0 ]; then
         
             export mode="$(< "${DC_tlt}/8.bk")"
             rm "${DC_tlt}/8.bk"; echo ${mode} > "${DC_tlt}/8.cfg"
             touch "${DM_tlt}"
+            
+            stts=${mode}
+            if [ -f "${DC_tlt}/9.cfg" ]; then
+                calculate_review "${topic}"
+                if [[ $((stts%2)) = 0 ]]; then
+                    if [ ${RM} -ge 180 -a ${stts} = 8 ]; then
+                        echo 10 > "${DC_tlt}/8.cfg"; touch "${DM_tlt}"
+                    elif [ ${RM} -ge 100 -a ${stts} -lt 8 ]; then
+                        echo 8 > "${DC_tlt}/8.cfg"; touch "${DM_tlt}"
+                    fi
+                else
+                    if [ ${RM} -ge 180 -a ${stts} = 7 ]; then
+                        echo 9 > "${DC_tlt}/8.cfg"; touch "${DM_tlt}"
+                    elif [ ${RM} -ge 100 -a ${stts} -lt 7 ]; then
+                        echo 7 > "${DC_tlt}/8.cfg"; touch "${DM_tlt}"
+                    fi
+                fi
+            fi
             echo "${topic}" > "$DC_s/4.cfg"
             checkt
             "$DS/mngr.sh" mkmn 1
