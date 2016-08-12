7d93ff5388a63f364234cc9fb25ba7ac37a717f6
robinpalat <robinpalat@gmail.com>
2016-08-12 15:25:45 -0300

    clipboard

--- ifs/clipw.sh
+++ ifs/clipw.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-id=$(xinput --list |grep -i -m 1 'mouse' |grep -o 'id=[0-9]\+' |grep -o '[0-9]\+')
-DT="/tmp/.idiomind-$USER"
-echo $$ > $DT/clipw
-
 cbwatch() {
     while [ 1 ]; do
         [ ! -e $DT/clipw ] && break
-        xclip -i /dev/null
+        xclip -selection clipboard /dev/null
         sleep 0.5
-        if [ -n "$(xclip -selection primary -o)" ]; then
-            stt1=$(xinput --query-state $id |grep 'button\[' |sort)
-            while true; do
-                stt2=$(xinput --query-state $id |grep 'button\[' |sort)
-                if grep 'up' <<< $(comm -13 <(echo "$stt1") <(echo "$stt2")); then break; fi
-                sleep 0.2
-                if grep 'down' <<< $(comm -13 <(echo "$stt1") <(echo "$stt2")); then break; fi
-            done
-            "/usr/share/idiomind/add.sh" new_items "" 2 "$(xclip -selection primary -o)"
+        if [[ -n "$(xclip -selection clipboard -o)" ]]; then
+            idiomind add "$(xclip -selection clipboard -o)"
+            xclip -selection clipboard /dev/null
         fi
     done
     return 0
 }
 
-cbwatch
+DT="/tmp/.idiomind-$USER"
+
+if [[ $1 =  1 ]]; then
+    yad --form --title="$(gettext "Clipboard watcher")" \
+    --name=Idiomind --class=Idiomind \
+    --text="$(gettext "Deactivate Clipboard watcher?")" \
+    --window-icon=idiomind \
+    --fixed --skip-taskbar --center --on-top \
+    --width=270 --height=90 --borders=4 \
+    --button="$(gettext "No")":2 \
+    --button="$(gettext "Yes")":1
+    ret="$?"
+    if [ $ret = 1 ]; then
+        source /usr/share/idiomind/default/c.conf
+        sed -i "s/clipw=.*/clipw=\"FALSE\"/g" "$DC_s/1.cfg"
+        [ -f $DT/clipw ] && rm -f $DT/clipw
+        exit 1 & "$DS/add.sh" new_items
+    fi
+else
+    #notify-send -i idiomind "$(gettext "Clipboard watcher active")" " " -t 10000
+    echo $$ > $DT/clipw
+    cbwatch
+fi
