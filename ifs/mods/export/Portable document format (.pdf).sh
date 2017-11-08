c3a783384f02b9a36d7ffecdab74f732a35bc46d
Robin <robinpalat@gmail.com>
2017-11-08 16:46:01 -0300

    update

--- ifs/mods/export/Portable document format (.pdf).sh
+++ ifs/mods/export/Portable document format (.pdf).sh
@@ -75,7 +75,7 @@ mkhtml() {
         if [ -n "${trgt}" -a -n "${srce}" -a -n "${type}" ]; then
         
             fimg="${DM_tlt}/images/${trgt,,}.jpg"
-            [ ! -e "$fimg" ] && fimg="$imagesdir/${trgt,,}-0.jpg"
+            [ ! -e "$fimg" ] && fimg="$imagesdir/${trgt,,}-1.jpg"
             if [ -e "$fimg" ]; then
                 img_small="<img class=\"simg\" src=\"$fimg\" width=100px></img><br>"
                 img_large="<img class=\"simg\" src=\"$fimg\" width=150px></img><br>"
